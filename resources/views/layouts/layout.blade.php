88e5ed1c7386f60fc454bc018bff93da599e11bd
Vasin Sermsampan <shinasaki07@gmail.com>
2017-08-13 02:44:48 +0700

    description

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,6 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="google" content="notranslate" />
+        <meta name="description" content="@lang('meta.description')" />
         <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
